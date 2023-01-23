b082d44e702ccfec42beb8165eba727c1deb5ff9
yongun2 <djkye0001@naver.com>
2023-01-23 11:59:08 +0900

    [feat] 메인 화면 스크린, 컨테이너

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainContainer from "../container/MainContainer";
+
+function MainScreen(props) {
+    return (
+        <>
+            {/* header container */}
+            <MainContainer />
+            {/* footer container */}
+        </>
+    );
+}
+
+export default MainScreen;
