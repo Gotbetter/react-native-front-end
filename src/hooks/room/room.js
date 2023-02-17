30a59ec58e8208e89a9d554d6760dc3a03ebac4b
yongun2 <djkye0001@naver.com>
2023-02-17 13:59:31 +0900

    [feat] 방 불러오기 hook

---
+++ src/hooks/room/room.js
@@ -0,0 +1,24 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
+import {fetchRooms} from "../../module/room";
+import {useIsFocused} from "@react-navigation/native";
+
+
+export const useFetchRoom = () => {
+
+    const dispatch = useDispatch();
+    const isFocused = useIsFocused();
+
+    const {rooms, loading} = useSelector(({auth, room}) => ({
+        rooms: room.rooms,
+        loading: room.loading.ROOM_FETCH,
+    }));
+
+    useEffect(() => {
+        dispatch(fetchRooms());
+    }, [isFocused]);
+
+
+
+    return [rooms, loading];
+}
