d49eb4e1fba67d0a0f3f961526a97ac0937fe8b9
yongun2 <djkye0001@naver.com>
2023-02-18 15:23:32 +0900

    [feat] plan reducer 추가

--- src/module/store.js
+++ src/module/store.js
@@ -3,10 +3,12 @@ import {logger} from "redux-logger/src";
 import ReduxThunk from 'redux-thunk';
 import auth from "./auth";
 import room from "./room";
+import plans from "./plans";
 export const store = configureStore({
     reducer: {
         auth,
         room,
+        plans,
     },
     middleware: [ReduxThunk, logger],
     devTools: window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
