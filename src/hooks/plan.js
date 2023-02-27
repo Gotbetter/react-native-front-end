ece4996d08792cafbc90602facf18305c897289c
yongun2 <djkye0001@naver.com>
2023-02-27 12:45:35 +0900

    [feat] 계획 과련 훅, api

---
+++ src/hooks/plan.js
@@ -0,0 +1,69 @@
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect, useState} from "react";
+import {useIsFocused} from "@react-navigation/native";
+import {fetchDislikeInfo, fetchPlanAndDetailPlan} from "../module/room";
+
+
+export function useWeekSelector() {
+
+    const {roomInfo} = useSelector(({room}) => room);
+    const [weekList, setWeekList] = useState([]);
+    const [clickedWeek, setClickedWeek] = useState(1); // 현재 클릭한 week
+
+
+    useEffect(() => {
+        /** 전체 주차 설정 **/
+        let next = [];
+        for (let i = 1; i <= roomInfo.week; ++i) {
+            next.push(i);
+        }
+        setWeekList(next);
+
+        /** 현재 클릭한 주차 설정 **/
+        setClickedWeek(roomInfo.current_week);
+
+    }, []);
+
+    /**
+     * weekList: WeekList 컴포넌트에서 (x주차)를 생성할때 필요한 배열
+     * clickedWeek: 현재 클릭한 주차
+     * setClickedWeek: clickedWeek setter
+     */
+    return [weekList, clickedWeek, setClickedWeek];
+
+}
+
+export function usePlanner(planner) {
+
+    const {user} = useSelector(({auth}) => auth);
+    const [isMyPlan, setIsMyPlan] = useState(null);
+
+    useEffect(() => {
+        /** 현재 계획이 자신이 세운 계획인지 확인 **/
+        setIsMyPlan((user.user_id === planner.user_id));
+
+    }, [planner]);
+
+
+    return isMyPlan;
+}
+
+
+export function useFetchPlanAndDetailPlans(participant_id, clickedWeek) {
+
+    const dispatch = useDispatch();
+
+    const {plan, detailPlans, planDislikeInfo} = useSelector(({room}) => room);
+
+    useEffect(() => {
+        dispatch(fetchPlanAndDetailPlan({participant_id, week: clickedWeek}));
+    }, [dispatch, participant_id, clickedWeek]);
+
+    /**
+     * plans: 현재 계획에 대한 전반적 정보
+     * planDislikeInfo: 계획에 대한 싫어요 수, 체크 유무
+     * detailPlans: 세부 계획
+     */
+    return [plan, planDislikeInfo, detailPlans];
+
+}
--- src/lib/plans.js
+++ src/lib/plans.js
@@ -1,7 +1,10 @@
 import client from "./client";
 
 
-export const fetchPlans = ({participant_id, week}) =>
+export const createPlan = (participant_id) =>
+    client.post(`/plans`, {participant_id});
+
+export const fetchPlan = ({participant_id, week}) =>
     client.get(`/plans/${participant_id}`, {params: {week}});
 
 export const fetchDetailPlan = (plan_id) =>
@@ -11,7 +14,7 @@ export const createDetailPlan = ({plan_id, content}) =>
     client.post(`/plans/${plan_id}/details`,{content});
 
 export const updateDetailPlan = ({plan_id, detail_plan_id, content}) =>
-    client.put(`/plans/${plan_id}/details/${detail_plan_id}`,{content});
+    client.patch(`/plans/${plan_id}/details/${detail_plan_id}`,{content});
 
 export const deleteDetailPlan = ({plan_id, detail_plan_id}) =>
     client.delete(`/plans/${plan_id}/details/${detail_plan_id}`);
@@ -22,6 +25,8 @@ export const fetchPlanDislike = (plan_id) =>
 
 export const planDislike = (plan_id) =>
     client.post(`/plans/${plan_id}/dislike`, {});
+export const planDislikeCancel = (plan_id) =>
+    client.delete(`/plans/${plan_id}/dislike`, {});
 
 export const deletePlanDislike = (plan_id) =>
     client.delete(`/plans/${plan_id}/dislike`);
