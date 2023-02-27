bf4a0e504c8acf2116bd74f4c505ef3ba2492e94
yongun2 <djkye0001@naver.com>
2023-02-27 12:41:04 +0900

    [feat] PlanScreen 컴포넌트 분해

---
+++ src/components/plans/plan/DislikeEvaluation.js
@@ -0,0 +1,37 @@
+import {View, TouchableOpacity, StyleSheet} from "react-native";
+import Icon from "react-native-vector-icons/Entypo";
+import {heightPercentageToDP as hp, widthPercentageToDP as wp} from "react-native-responsive-screen";
+
+
+function DislikeEvaluation({onPress, planDislikeInfo}) {
+    const {checked, dislike_count} = planDislikeInfo
+    return (
+        <View style={[styles.dislike_button, checked === true ? styles.checked : null]}>
+            <TouchableOpacity onPress={onPress}>
+                <Icon name="thumbs-down" color="white" size={hp(6)}/>
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create(
+    {
+        dislike_button: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginLeft: wp(5),
+            marginRight: wp(5),
+            borderRadius: 20,
+            borderWidth: 5,
+            borderColor: '#BFBFBF',
+            backgroundColor: '#000000',
+        },
+        checked: {
+            backgroundColor: 'red',
+        },
+    }
+)
+
+export default DislikeEvaluation;
