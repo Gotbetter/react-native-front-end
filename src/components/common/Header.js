38d4d98c15578a977fb790e8a4107dff0e36233a
yongun2 <djkye0001@naver.com>
2023-04-06 16:47:55 +0900

    [UI] Header 비율 조정

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,29 +1,37 @@
 import React from 'react';
-
 import {StyleSheet, View} from "react-native";
 import Logo from "./Logo";
 import Icon from "react-native-vector-icons/Entypo";
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
 function Header({onPress}) {
     return (
         <View style={styles.container}>
             <View style={styles.blank}/>
-            <Logo/>
-            <Icon name="menu" size={40} style={styles.menu} onPress={onPress}/>
+            <View style={styles.header_container}>
+                <View style={{width: 40}}/>
+                <Logo/>
+                <Icon name="menu" size={40} style={styles.menu} onPress={onPress}/>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        width: "100%",
+        height: "10%",
+    },
+    header_container: {
+        backgroundColor: "#ffffff",
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
         width: "100%",
-        marginTop: "2%",
+        height: hp(7),
     },
     blank: {
-        width: wp(10),
+        height: hp(2),
     },
     menu: {
         marginRight: "2%",
