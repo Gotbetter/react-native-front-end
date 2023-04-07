8d44991126d16f030a6ff7533b864da916693d85
yongun2 <djkye0001@naver.com>
2023-04-07 22:25:33 +0900

    [UI] 계획 확인 화면 컴포넌트

--- src/components/plans/plan/WeekList.js
+++ src/components/plans/plan/WeekList.js
@@ -1,91 +1,59 @@
 import React from 'react';
 import {ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import {heightPercentageToDP as hp, widthPercentageToDP as wp} from "react-native-responsive-screen";
+import Feather from "react-native-vector-icons/Feather";
 
-function WeekList({plannerName, weekList, clickedWeek, onPress}) {
+function WeekList({}) {
+
+    const weekList = [1, 2, 3, 4];
+
     return (
-        <View style={{flex: 1, flexDirection: 'row'}}>
-            <View style={{flex: 0.5}}/>
-            <View style={styles.user_container}>
-                <Text style={styles.user_name_text}>{plannerName}</Text>
-            </View>
+        <View style={styles.container}>
+            <TouchableOpacity>
+                <Feather name="chevron-left" style={styles.close_icon} size={35} />
+            </TouchableOpacity>
             <View style={styles.week_container}>
-                <ScrollView horizontal={true} style={{flex: 1}}>
-                    {
-                        weekList && (
-                            weekList.map((week) => (
-                                <TouchableOpacity key={week}
-                                                  style={clickedWeek === week ? styles.current_week : styles.non_current_week}
-                                                  onPress={() => onPress(week)}>
-                                    <Text style={clickedWeek === week ? styles.current_week_text : styles.week_text}>
-                                        {week}주차
-                                    </Text>
-                                </TouchableOpacity>
-                            )))
-                    }
-                </ScrollView>
+                {
+                    weekList.map((week,index) => (
+                        <TouchableOpacity key={index} style={styles.week}>
+                            <Text style={styles.text}>{week}주차</Text>
+                        </TouchableOpacity>
+                    ))
+                }
             </View>
-            <View style={{flex: 0.5}}/>
+            <TouchableOpacity>
+                <Feather name="chevron-right" style={styles.close_icon} size={35} />
+            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create(
     {
-        user_container: {
-            flex: 2,
-            backgroundColor: '#DFDFDF',
-            borderTopLeftRadius: 20,
-            borderBottomLeftRadius: 20,
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        user_name_text: {
-            color: '#000000',
-            textAlign: 'center',
-            fontSize: 20,
+        container: {
+            width: "100%",
+            height: "8%",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between"
         },
         week_container: {
-            flex: 7,
-            borderTopRightRadius: 20,
-            borderBottomRightRadius: 20,
-            borderWidth: 5,
-            borderColor: '#BFBFBF',
-            alignItems: 'center',
-            justifyContent: 'center',
-            flexDirection: 'row',
-        },
+            flexDirection: "row",
+            justifyContent: "space-around",
+            width: "80%",
 
-        non_current_week: {
-            width: hp(8),
-            height: hp(8),
-            marginLeft: wp(1),
-            borderRadius: 30,
-            backgroundColor: '#FFFFFF',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderWidth: 5,
-            borderColor: '#BFBFBF',
         },
-        current_week_text: {
-            color: '#FFFFFF',
-            textAlign: 'center',
-            fontSize: 15,
+        week :{
+            width: wp(14),
+            height: hp(5),
+            justifyContent: "center",
+            alignItems: "center",
+            borderRadius: 20,
+            backgroundColor: "#B4C8BB",
+
         },
-        current_week: {
-            width: hp(8),
-            height: hp(8),
-            marginLeft: wp(1),
-            borderRadius: 30,
-            backgroundColor: '#000000',
-            justifyContent: 'center',
-            alignItems: 'center',
-            borderWidth: 5,
-            borderColor: '#BFBFBF',
-        },
-        week_text: {
-            color: '#000000',
-            textAlign: 'center',
+        text: {
+            color: "#ffffff",
             fontSize: 15,
         },
     }
