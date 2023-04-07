b1bff44628701211bfdf99f4579a7a8748fc48e9
yongun2 <djkye0001@naver.com>
2023-04-07 18:03:26 +0900

    [UI] 스터디룸 footer

--- src/components/room/main/RoomFooter.js
+++ src/components/room/main/RoomFooter.js
@@ -4,17 +4,37 @@ import Feather from "react-native-vector-icons/Feather";
 import Entypo from "react-native-vector-icons/Entypo";
 import {AntDesign} from "@expo/vector-icons";
 
-function RoomFooter({isReader}) {
+function RoomFooter({isReader, showHelp, showInfo, showInvite, showRank}) {
+
+    const footerItems = [
+        {
+            id: "Help",
+            icon: <AntDesign name="questioncircleo" size={32} onPress={showHelp}/>
+        },
+        {
+            id: "Info",
+            icon: <Feather name="info" size={35} onPress={showInfo}/>
+        },
+        {
+            id: "Invite",
+            icon: isReader ? <Feather name="user-plus" size={35} onPress={showInvite}/> : null
+        },
+        {
+            id: "Rank",
+            icon: <Entypo name="bar-graph" size={35} onPress={showRank}/>
+        },
+
+    ]
 
     return (
         <View style={styles.container}>
-            <AntDesign name="questioncircleo" size={32}/>
-            <Feather name="info" size={35}/>
             {
-                isReader ? <Feather name="user-plus" size={35}/> : null
+                footerItems.map(item => (
+                    <View key={item.id}>
+                        {item.icon}
+                    </View>
+                ))
             }
-            <Entypo name="bar-graph" size={35}/>
-
         </View>
     );
 }
