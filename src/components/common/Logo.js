e159a5ea86e588861645e4ce4a3c53642ba90ff6
yongun2 <djkye0001@naver.com>
2023-04-10 03:16:30 +0900

    [UI] logo 크기 props 설정 및 템플릿 적용

--- src/components/common/Logo.js
+++ src/components/common/Logo.js
@@ -2,18 +2,11 @@ import React from 'react';
 import {Image, StyleSheet} from "react-native";
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
-function Logo() {
+function Logo({size}) {
     return (
         <Image source={require('../../../assets/images/logo.png')} resizeMode='contain'
-               style={styles.logo_image}/>
+               style={{width: size}}/>
     );
 }
 
-const styles = StyleSheet.create(
-    {
-        logo_image: {
-            width: wp(40),
-        },
-    }
-);
 export default Logo;
