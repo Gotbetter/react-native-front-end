edf8cc6a6860dcbb26009eb3dc1193b4bf29e391
yongun2 <djkye0001@naver.com>
2023-06-13 01:57:59 +0900

    [FEAT] name, value, onChange, onBlur prop 추가

--- src/components/common/BasicTextInput.js
+++ src/components/common/BasicTextInput.js
@@ -2,11 +2,14 @@ import React from 'react';
 import {StyleSheet, TextInput} from "react-native";
 import {heightPercentageToDP as hp, widthPercentageToDP as wp} from "react-native-responsive-screen";
 
-function BasicTextInput({style, placeholder, secure=false, dark=false}) {
+function BasicTextInput({style,dark=false, placeholder, secure=false, name, value, onChangeText, onBlur=null}) {
     return (
         <TextInput style={[styles.container, {backgroundColor: dark ? "#EEEEEE" : "#FFFFFF"}, style]}
                    placeholder={placeholder} placeholderTextColor={"#BDBDBD"}
                    secureTextEntry={secure}
+                   value={value}
+                   onChange={(e) => onChangeText(e, name)}
+                   onBlur={onBlur}
         />
     );
 }
@@ -14,6 +17,7 @@ function BasicTextInput({style, placeholder, secure=false, dark=false}) {
 const styles = StyleSheet.create(
     {
         container: {
+            borderRightWidth: 1,
             width: wp(90),
             height: hp(7),
             padding: 10,
