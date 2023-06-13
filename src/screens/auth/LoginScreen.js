17964a39e9842cb1314d3c3ff1a8603786445dbe
yongun2 <djkye0001@naver.com>
2023-06-13 13:39:45 +0900

    [REFACTOR] 로그인 로직 모두 리듀서에 이동

--- src/screens/auth/LoginScreen.js
+++ src/screens/auth/LoginScreen.js
@@ -1,89 +1,39 @@
 import {SafeAreaView, StyleSheet, View} from "react-native";
-import {useEffect, useState,} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {login, resetAllError, resetStatus, setError} from "../../module/auth";
+import {login, onChangeLoginRequest, resetAllStatusAndError} from "../../module/auth";
 import Logo from "../../components/common/Logo";
-import {useIsFocused} from "@react-navigation/native";
 import PreventRollUpView from "../../components/common/PreventRollUpView";
 import BasicTextInput from "../../components/common/BasicTextInput";
 import ActionButton from "../../components/common/ActionButton";
 import {RFValue} from "react-native-responsive-fontsize";
 import LoginSubOption from "../../components/auth/LoginSubOption";
 import ErrorMessage from "../../components/common/ErrorMessage";
+import {useEffect} from "react";
+import Toast from "react-native-root-toast";
 
 function LoginScreen({navigation}) {
 
     const dispatch = useDispatch();
-    /*const {status, error} = useSelector(({auth}) => ({
-        status: auth.status.LOGIN,
-        error: auth.error.LOGIN,
-    }));*/
 
-    const isFocused = useIsFocused();
-    const [errorMessage, setErrorMessage] = useState("");
-
-    /*useEffect(() => {
-        if (isFocused) {
-            setErrorMessage("");
-            dispatch(resetAllError());
-            dispatch(resetStatus("DUPLICATE_CHECKED"));
-            dispatch(resetStatus("PASSWORD_CONFIRMED"));
-        }
-    }, [isFocused])*/
-
-    /*useEffect(() => {
-
-        /!** status === 200일 경우 로그인 상태*!/
-        if (status === 200) {
-            navigation.navigate('main');
-        }
-
-        /!** 아이디 또는 비밀번호 틀렸을시 오류 메세지 출력 *!/
-        if (status === 404) {
-            setErrorMessage("아이디 비밀번호를 확인하세요");
-            dispatch(resetStatus("LOGIN"));
-        }
-
-    }, [dispatch, status]);*/
-
-    const [request, setRequest] = useState({
-        auth_id: '',
-        password: '',
-    });
-
+    const {request, status, error, LOGIN_FAILED_MESSAGE} = useSelector(({auth}) => (auth.login));
     const {auth_id, password} = request;
 
-    const onChange = (targetName, e) => {
-        const {text} = e.nativeEvent;
-        const next = {
-            ...request,
-            [targetName]: text,
-        };
-        setRequest(next);
+    useEffect(() => {
+        if (status.LOGIN_SUCCESS) {
+            navigation.navigate('main');
+            Toast.show("로그인 성공", {duration: Toast.durations.SHORT});
+        }
+
+        return () => dispatch(resetAllStatusAndError());
+    }, [status.LOGIN_SUCCESS]);
+
+    const onChange = (e, targetName) => {
+        dispatch(onChangeLoginRequest({targetName, value: e.nativeEvent.text}));
     };
 
     /** 로그인 버튼 눌렀을 때 **/
     const onPressLogin = () => {
-        let flag = true;
-        for (const requestKey in request) {
-            if (request[requestKey] === '') {
-                flag = false;
-                break;
-            }
-        }
-
-        if (flag === false) {
-            setErrorMessage("모든 정보를 입력하세요");
-            dispatch(setError("LOGIN"));
-        } else {
-            dispatch(login(request));
-            const resetRequest = {
-                auth_id: '',
-                password: '',
-            };
-            setRequest(resetRequest);
-            setErrorMessage("");
-        }
+        dispatch(login());
     };
 
     return (
@@ -94,10 +44,12 @@ function LoginScreen({navigation}) {
                 </View>
                 <View style={styles.main_container}>
                     <View style={styles.gap}>
-                        <BasicTextInput placeholder={"아이디"}/>
-                        <BasicTextInput placeholder={"비밀번호"} secure={true}/>
-                        <ErrorMessage message={"아이디와 비밀번호를 확인하세요"}/>
-                        <ActionButton name={"로그인"}/>
+                        <BasicTextInput placeholder={"아이디"} name={"auth_id"} value={auth_id}
+                                        onChangeText={onChange}/>
+                        <BasicTextInput placeholder={"비밀번호"} secure={true} name={"password"} value={password}
+                                        onChangeText={onChange}/>
+                        <ErrorMessage visible={error.LOGIN_FAILED} message={LOGIN_FAILED_MESSAGE}/>
+                        <ActionButton name={"로그인"} onPress={onPressLogin}/>
                         <LoginSubOption navigation={navigation}/>
                     </View>
                 </View>
