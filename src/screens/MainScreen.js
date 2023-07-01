2427e2475008bcc22df9f7336c988cf6bc6f1747
yongun2 <djkye0001@naver.com>
2023-07-01 13:00:22 +0900

    [DESIGN] 홈 화면 디자인 변경

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,29 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {useFetchRoomList, useFetchUser} from "../hooks/room";
-import {fetchUser, logout} from "../module/auth";
-import {Platform, RefreshControl, SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View} from "react-native";
-import {useIsFocused, useNavigation} from "@react-navigation/native";
-import {fetchRooms, refreshRooms, resetRoom, resetRoomCreateRequest} from "../module/room";
+import {useFetchRoomList} from "../hooks/room";
+import {Platform, RefreshControl, SafeAreaView, ScrollView, StatusBar, StyleSheet, View} from "react-native";
+import {useIsFocused} from "@react-navigation/native";
+import {refreshRooms, resetRoom, resetRoomCreateRequest} from "../module/room";
 import RoomItem from "../components/main/RoomItem";
 import {resetPlanAndDetailPlan} from "../module/plan";
-import Header from "../components/common/Header";
 import Footer from "../components/common/home/Footer";
-import MenuModal from "../components/main/MenuModal";
 import {RFValue} from "react-native-responsive-fontsize";
 import ErrorMessage from "../components/common/ErrorMessage";
+import Logo from "../components/common/Logo";
 
 function MainScreen() {
 
     const dispatch = useDispatch();
-    const navigation = useNavigation();
     const isFocused = useIsFocused();
 
 
     const { loading, error, message } = useSelector(({room}) => room);
-    const [showMenu, setShowMenu] = useState(false);
-
-
     const roomList = useFetchRoomList();
 
 
@@ -37,15 +31,6 @@ function MainScreen() {
 
     }, [isFocused]);
 
-    /** 로그아웃 눌렀을 경우 **/
-    const onPressLogout = () => {
-        dispatch(logout())
-            .unwrap()
-            .then(() => {
-                setShowMenu(false);
-                navigation.reset({routes: [{name: 'login'}]});
-            });
-    };
 
     /** 방 리스트 새로고침 **/
     const onRefresh = () => {
@@ -54,10 +39,10 @@ function MainScreen() {
 
     return (
         <SafeAreaView style={styles.container}>
-            <Header onPress={() => setShowMenu(true)}/>
+            <View style={styles.logo_container}>
+                <Logo size={RFValue(200)}/>
+            </View>
             <View style={styles.contents_container}>
-
-                <Text style={styles.contents_title}>스터디 룸</Text>
                 <View style={styles.contents_scroll_view_container}>
                     <ErrorMessage visible={error.ROOM_REFRESHING_FAILED} message={message.ROOM_REFRESHING_FAILED_MESSAGE}/>
                     <ScrollView refreshControl={
@@ -72,13 +57,17 @@ function MainScreen() {
                 </View>
             </View>
             <Footer/>
-            <MenuModal show={showMenu} onPressClose={() => setShowMenu(false)} onPressLogout={onPressLogout}/>
         </SafeAreaView>
     );
 }
 
 const styles = StyleSheet.create(
     {
+        logo_container: {
+            flex: 1,
+            justifyContent: "flex-end",
+            alignItems: "center",
+        },
         container: {
             flex: 1,
             backgroundColor: "white",
@@ -93,6 +82,7 @@ const styles = StyleSheet.create(
         },
         contents_scroll_view_container: {
             flex: 1,
+            backgroundColor: "#F5F5F5",
             alignItems: "center",
         }
 
